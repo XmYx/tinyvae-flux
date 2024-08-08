fab990aa24d63170ed12468119a34750fcaa4a9e
Miklos Nagy <miklos.mnagy@gmail.com>
2024-08-08 18:08:54 +0200

    Update train_flux_tinyvae.py
    add variants

--- train_flux_tinyvae.py
+++ train_flux_tinyvae.py
@@ -77,69 +77,6 @@ class TinyAutoEncoder(nn.Module):
     def unscale_latents(x):
         return x.sub(TinyAutoEncoder.latent_shift).mul(2 * TinyAutoEncoder.latent_magnitude)
 
-# def conv(n_in, n_out, **kwargs):
-#     return nn.Conv2d(n_in, n_out, 3, padding=1, **kwargs)
-#
-# class Clamp(nn.Module):
-#     def forward(self, x):
-#         return torch.tanh(x / 3) * 3
-#
-# class Block(nn.Module):
-#     def __init__(self, n_in, n_out):
-#         super().__init__()
-#         self.conv = nn.Sequential(conv(n_in, n_out), nn.ReLU(), conv(n_out, n_out), nn.ReLU(), conv(n_out, n_out))
-#         self.skip = nn.Conv2d(n_in, n_out, 1, bias=False) if n_in != n_out else nn.Identity()
-#         self.fuse = nn.ReLU()
-#     def forward(self, x):
-#         return self.fuse(self.conv(x) + self.skip(x))
-#
-# class Encoder(nn.Module):
-#     def __init__(self, channels, latent_channels):
-#         super().__init__()
-#         layers = [conv(3, channels[0]), Block(channels[0], channels[0])]
-#         for i in range(len(channels) - 1):
-#             layers += [conv(channels[i], channels[i + 1], stride=2, bias=False), Block(channels[i + 1], channels[i + 1])]
-#         layers += [conv(channels[-1], latent_channels, stride=1, bias=False)]
-#         self.encoder = nn.Sequential(*layers)
-#
-#     def forward(self, x):
-#         return self.encoder(x)
-#
-# class Decoder(nn.Module):
-#     def __init__(self, channels, latent_channels):
-#         super().__init__()
-#         layers = [Clamp(), conv(latent_channels, channels[0]), nn.ReLU()]
-#         for i in range(len(channels) - 1):
-#             layers += [Block(channels[i], channels[i]), nn.Upsample(scale_factor=2), conv(channels[i], channels[i + 1], bias=False)]
-#         layers += [Block(channels[-1], channels[-1]), nn.Upsample(scale_factor=1), conv(channels[-1], 3)]
-#         self.decoder = nn.Sequential(*layers)
-#
-#     def forward(self, x):
-#         return self.decoder(x)
-#
-# class TinyAutoEncoder(nn.Module):
-#     latent_magnitude = 0.3611
-#     latent_shift = 0.1159
-#
-#     def __init__(self, encoder_path=None, decoder_path=None, latent_channels=16, channels=None):
-#         super().__init__()
-#         if channels is None:
-#             channels = [128, 256, 512, 1024]
-#         self.encoder = Encoder(channels, latent_channels)
-#         self.decoder = Decoder(channels[::-1], latent_channels)
-#         if encoder_path is not None:
-#             self.encoder.load_state_dict(torch.load(encoder_path, map_location="cpu"))
-#         if decoder_path is not None:
-#             self.decoder.load_state_dict(torch.load(decoder_path, map_location="cpu"))
-#
-#     @staticmethod
-#     def scale_latents(x):
-#         return x.div(2 * TinyAutoEncoder.latent_magnitude).add(TinyAutoEncoder.latent_shift).clamp(0, 1)
-#
-#     @staticmethod
-#     def unscale_latents(x):
-#         return x.sub(TinyAutoEncoder.latent_shift).mul(2 * TinyAutoEncoder.latent_magnitude)
-
 class ImageFolderDataset(Dataset):
     def __init__(self, folder_path, transform=None):
         self.folder_path = folder_path
@@ -249,20 +186,28 @@ def main(data_folder, output_folder, epochs=100000, batch_size=8, learning_rate=
 
     # Define the size variants
     size_variants = {
+        'zepto': [1,2,2,4],
+        'yocto': [16,8,8,16],
+        'zeptometer': [2, 4, 8, 16],
+        'yoctometer': [4, 8, 8, 16],
+        'angstrom': [4, 8, 16, 32],
+        'attometer': [8, 16, 16, 32],
+        'nanometer': [8, 16, 32, 64],
+        'micrometer': [16, 32, 32, 64],
         'pico': [16, 32, 64, 64],
         'nano': [16, 32, 64, 128],
         'micro': [24, 48, 96, 192],
         'tiny': [32, 64, 128, 256],
         'small': [64, 128, 256, 512],
-        'normal': [128, 256, 512, 1024]
+        'normal': [128, 256, 512, 1024],
+        'large': [256, 512, 1024, 2048],
     }
-    channels = size_variants.get('pico', size_variants['normal'])
+    channels = size_variants.get('yocto', size_variants['normal'])
 
     model = TinyAutoEncoder(channels=channels).to(device)
 
-
-    # model.decoder.load_state_dict(torch.load('/home/mix/Playground/flux_base/output_100k/tiny_decoder_epoch_360.pth'))
-    # model.encoder.load_state_dict(torch.load('/home/mix/Playground/flux_base/output_100k/tiny_encoder_epoch_360.pth'))
+    # model.decoder.load_state_dict(torch.load('/home/mix/Playground/flux_base/output_dynamic/tiny_decoder_epoch_2740.pth'))
+    # model.encoder.load_state_dict(torch.load('/home/mix/Playground/flux_base/output_dynamic/tiny_encoder_epoch_2740.pth'))
 
     vae = AutoencoderKL.from_pretrained("black-forest-labs/FLUX.1-schnell", subfolder='vae', torch_dtype=torch.float16).to(device)
     processor = VaeImageProcessor(vae_scale_factor=16, vae_latent_channels=16)
